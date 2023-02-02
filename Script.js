57b3c436ef8813dd37d636469acbadf0b6896feb
Simone <ssimone12@gmail.com>
2023-02-02 11:23:35 +0100

    first commit

---
+++ Script.js
@@ -0,0 +1,14 @@
+/* SIDEN VISES */
+function init() {
+  console.log("init");
+}
+
+/* COMPUTER VALG */
+function getRandomChoice() {
+  console.log("GetRandomChoise");
+}
+
+/* USER VALG */
+function userChoise() {
+  console.log("GetRandomChoise");
+}
